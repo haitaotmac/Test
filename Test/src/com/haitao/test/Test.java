21131b486abba4a49c2586684a76c1e49f0f8105
haitao <haitaojodan@126.com>
2016-08-09 08:01:21 +0800

    modif Test by man02

--- Test/src/com/haitao/test/Test.java
+++ Test/src/com/haitao/test/Test.java
@@ -2,6 +2,8 @@ package com.haitao.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("sdfffffffffffff");
+		//sddfasdf
+		System.out.println("sd11111111111fffffffffffff");
+		//bbb
 	}
 }
